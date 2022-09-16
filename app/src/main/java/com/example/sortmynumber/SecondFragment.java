9bb77fe57173fb9e5e8a8536d9dc9c66aa515a0a
Arati Maurya <aratimaurya@csu.fullerton.edu>
2022-09-15 19:57:08 -0700

    Added input validation,clear & Exit button functionality

--- app/src/main/java/com/example/sortmynumber/SecondFragment.java
+++ app/src/main/java/com/example/sortmynumber/SecondFragment.java
@@ -1,5 +1,6 @@
 package com.example.sortmynumber;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,8 +9,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.sortmynumber.databinding.FragmentSecondBinding;
 
@@ -34,14 +35,61 @@ public class SecondFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
+
+        binding.exitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+                //               NavHostFragment.findNavController(SecondFragment.this)
+//                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+                exitApp(view);
+
+            }
+
+            public void exitApp(View view) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+              //  builder.setTitle("Exit Message");
+                builder.setMessage("Do you want to exit the app?");
+                builder.setPositiveButton("Yes. Exit now!", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                        getActivity().finish();
+                    }
+
+                });
+                builder.setNegativeButton("Not now", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                        dialogInterface.dismiss();
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
             }
 
         });
+
+        binding.clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                clearText(view);
+
+            }
+
+            public void clearText(View view) {
+                EditText editText = (EditText) getView().findViewById(R.id.inputEditText);
+                TextView textView1 = getView().findViewById(R.id.textView1);
+
+                editText.setText("");
+                textView1.setText("");
+
+            }
+
+        });
+
+
         binding.sortButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -49,37 +97,17 @@ public class SecondFragment extends Fragment {
             }
 
             public void sendMessage(View view) {
-                EditText editText = (EditText) getView().findViewById(R.id.editTextTextPersonName);
+                EditText editText = (EditText) getView().findViewById(R.id.inputEditText);
                 TextView textView1 = getView().findViewById(R.id.textView1);
 
 
                 try {
                     String myString = editText.getText().toString();
+                    Integer[] numberArr = inputValidation(myString);
+                    if (numberArr == null || numberArr.length == 0)
+                        return;
+                    String finalString = numberSorting(numberArr);
 
-                    String[] myStringList = myString.split(" ");
-
-                    ArrayList<Integer> numberArr = new ArrayList<Integer>();
-
-                    for (int i = 0; i < myStringList.length; i++) {
-                        if (!myStringList[i].equals("")) {
-                            numberArr.add(Integer.parseInt(myStringList[i]));
-                        }
-                    }
-                    Integer[] numberList = numberArr.toArray(new Integer[0]);
-                    String finalString = "Input Array: " +
-                            Arrays.toString(numberList).replaceAll("\\[|\\]|,|\\s", " ") + "\n"
-                            + "Insertion Sort (Intermediate Steps)";
-                    int size = numberList.length;
-                    for (int i = 1; i < size; i++) {
-                        int keyNum = numberList[i];
-                        int j = i - 1;
-                        while (j >= 0 && numberList[j] > keyNum) {
-                            numberList[j + 1] = numberList[j];
-                            j = j - 1;
-                        }
-                        numberList[j + 1] = keyNum;
-                        finalString = finalString + "\n" + Arrays.toString(numberList).replaceAll("\\[|\\]|,|\\s", " ");
-                    }
                     textView1.setText(finalString);
 
                 } catch (Exception e) {
@@ -88,9 +116,85 @@ public class SecondFragment extends Fragment {
 
             }
 
+            public String numberSorting(Integer[] numberArr) {
+                String finalString = "Input Array: " +
+                        Arrays.toString(numberArr).replaceAll("\\[|\\]|,|\\s", " ") + "\n"
+                        + "Insertion Sort (Intermediate Steps)";
+                int size = numberArr.length;
+                for (int i = 1; i < size; i++) {
+                    int keyNum = numberArr[i];
+                    int j = i - 1;
+                    while (j >= 0 && numberArr[j] > keyNum) {
+                        numberArr[j + 1] = numberArr[j];
+                        j = j - 1;
+                    }
+                    numberArr[j + 1] = keyNum;
+                    finalString = finalString + "\n" + Arrays.toString(numberArr).replaceAll("\\[|\\]|,|\\s", " ");
+                }
+                return finalString;
+            }
+
+            public Integer[] inputValidation(String inputString) {
+                String[] myStringList = {};
+                // Integer[] numberList= new Integer[];
+                if (inputString.equals("")) {
+                    this.messageBox("Enter Numbers for Sorting");
+                    return null;
+                } else if (!inputString.matches("[0-9 ]+")) {
+                    this.messageBox("Enter space separated numbers only");
+                    return null;
+                }
+
+                myStringList = inputString.split(" ");
+
+                ArrayList<Integer> numberList = new ArrayList<Integer>();
+
+                for (int i = 0; i < myStringList.length; i++) {
+                    if (!myStringList[i].equals("")) {
+
+                        Integer num = Integer.parseInt(myStringList[i]);
+                        //check range
+                        if (num >= 0 && num <= 9) {
+                            numberList.add(num);
+                        } else {
+                            this.messageBox("Enter space separated numbers between 0 to 9");
+                            return null;
+                        }
+                    }
+                }
+                if (numberList.size() < 2) {
+                    this.messageBox("Enter minimum 2 numbers");
+                    return null;
+                } else if (numberList.size() > 8) {
+                    this.messageBox("Enter upto  8 numbers only ");
+                    return null;
+                }
+
+                Integer[] numberArr = new Integer[numberList.size()];
+                numberList.toArray(numberArr);
+
+                return numberArr;
+            }
+
+
+            public void messageBox(String message) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setTitle("Alert");
+                builder.setMessage(message);
+
+                // add a button
+                builder.setPositiveButton("OK", null);
+
+                // create and show the alert dialog
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+
         });
 
+
     }
+
 
     @Override
     public void onDestroyView() {
